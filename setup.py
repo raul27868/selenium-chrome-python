405e5fccf3c675e9e307ca69c16f0f741663859b
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2021-02-20 19:10:45 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='selenium-chrome-python',
+    version='1.0.0',
+    url='https://github.com/raul27868/selenium-chrome-python.git',
+    author='Raul Reyero',
+    author_email='raul.reyero.diez@gmail.com',
+    description='Multiples utilities',
+    packages=find_packages(),    
+    install_requires=[  ],
+)
+ 
