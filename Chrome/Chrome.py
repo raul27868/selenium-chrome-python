a50be199d78a91dce31032696ece8ceb72def078
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2021-02-20 19:23:54 +0100

    Update Chrome.py

--- Chrome/Chrome.py
+++ Chrome/Chrome.py
@@ -1,33 +1,6 @@
 
 
 
-from datetime import date
-from datetime import datetime
-import pytz  #Zonas horarias
- 
-
-import urllib #Convertir caracteres
-
-import time
-import json
-import os
-import sys
-import re
-import random
-import pprint     #pretty print json
-import inspect    #para saber el nombre de la función desde la que se llama
-
-from bs4 import BeautifulSoup
-
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
 
 #Identifica donde esta el driver de chrome
 EXECUTABLE_PATH = '/usr/bin/chromedriver'
